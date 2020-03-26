cfbdce247f17c014782816bb148aef8e141d5261
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-26 20:35:11 +0100

    More defaults

--- js/get_metadata.js
+++ js/get_metadata.js
@@ -38,7 +38,7 @@ async function main() {
 					"storage": storage,
 					"type": type,
 					"prefix": query.keyPrefix(),
-					"generate": 1
+					"generate": generate
 				})
 			}
 		}
@@ -61,10 +61,14 @@ function getGenerate(module, storage) {
 	let active_users = 5000;
 	let accounts = 100000;
 	let stakers = 20000;
+	let staking_history_depth = 84;
+	let staking_bonding_duration = 28;
 	let validators = 1000;
 	let voters = 50000;
 	let treasury = 1000;
 	let society = 150;
+	let parachains = 100;
+	let parathreads = 1000;
 
 	if (module == "system" && storage == "account") {
 		return accounts
@@ -106,6 +110,30 @@ function getGenerate(module, storage) {
 		return stakers
 	}
 
+	if (module == "staking" && storage == "erasStakers") {
+		return staking_history_depth
+	}
+
+	if (module == "staking" && storage == "erasStakersClipped") {
+		return staking_history_depth
+	}
+
+	if (module == "staking" && storage == "erasValidatorPrefs") {
+		return staking_history_depth
+	}
+
+	if (module == "staking" && storage == "erasValidatorReward") {
+		return staking_history_depth
+	}
+
+	if (module == "staking" && storage == "erasRewardPoints") {
+		return staking_history_depth
+	}
+
+	if (module == "staking" && storage == "erasTotalStake") {
+		return staking_history_depth
+	}
+
 	if (module == "staking" && storage == "validators") {
 		return validators
 	}
@@ -114,11 +142,19 @@ function getGenerate(module, storage) {
 		return (stakers / 1000)
 	}
 
+	if (module == "staking" && storage == "validatorSlashInEra") {
+		return staking_bonding_duration
+	}
+
+	if (module == "staking" && storage == "nominatorSlashInEra") {
+		return staking_bonding_duration
+	}
+
 	if (module == "session" && storage == "nextKeys") {
 		return stakers
 	}
 
-	if (module == "staking" && storage == "keyOwner") {
+	if (module == "session" && storage == "keyOwner") {
 		return accounts
 	}
 
@@ -146,7 +182,11 @@ function getGenerate(module, storage) {
 		return voters;
 	}
 
-	if (module == "electionsPhragmen" && storage == "voting") {
+	if (module == "electionsPhragmen" && storage == "votesOf") {
+		return voters;
+	}
+
+	if (module == "electionsPhragmen" && storage == "stakeOf") {
 		return voters;
 	}
 
@@ -154,7 +194,7 @@ function getGenerate(module, storage) {
 		return treasury;
 	}
 
-	if (module == "treasury" && storage == "proposals") {
+	if (module == "treasury" && storage == "tips") {
 		return treasury;
 	}
 
@@ -170,6 +210,54 @@ function getGenerate(module, storage) {
 		return sale_buyers;
 	}
 
+	if (module == "parachains" && storage == "code") {
+		return parachains;
+	}
+
+	if (module == "parachains" && storage == "heads") {
+		return parachains;
+	}
+
+	if (module == "parachains" && storage == "relayDispatchQueue") {
+		return parachains;
+	}
+
+	if (module == "parachains" && storage == "relayDispatchQueueSize") {
+		return parachains;
+	}
+
+	if (module == "slots" && storage == "deposits") {
+		return parachains;
+	}
+
+	if (module == "slots" && storage == "winning") {
+		return parachains;
+	}
+
+	if (module == "slots" && storage == "reservedAmounts") {
+		return parachains * 10;
+	}
+
+	if (module == "slots" && storage == "onboarding") {
+		return parachains;
+	}
+
+	if (module == "slots" && storage == "offboarding") {
+		return parachains;
+	}
+
+	if (module == "registrar" && storage == "pendingSwap") {
+		return parachains / 10;
+	}
+
+	if (module == "registrar" && storage == "paras") {
+		return parachains + parathreads;
+	}
+
+	if (module == "registrar" && storage == "debtors") {
+		return parathreads;
+	}
+
 	if (module == "utility" && storage == "multisigs") {
 		return active_users;
 	}
@@ -179,10 +267,6 @@ function getGenerate(module, storage) {
 	}
 
 	if (module == "identity" && storage == "superOf") {
-		return active_users;
-	}
-
-	if (module == "identity" && storage == "identityOf") {
 		return (active_users * 4);
 	}
 
