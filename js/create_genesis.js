c7930d2875c9625718788a1fd8a10cd8fd46579c
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-28 00:42:03 +0100

    update

--- js/create_genesis.js
+++ js/create_genesis.js
@@ -1,4 +1,6 @@
-var { xxhashAsHex } = require('@polkadot/util-crypto');
+var { xxhashAsHex, deriveHard, blake2AsHex, encodeAddress } = require('@polkadot/util-crypto');
+var { u8aToHex } = require('@polkadot/util');
+
 
 // Main function which needs to run at start
 function main() {
@@ -11,10 +13,19 @@ function main() {
 		for (storage of storageMetadata) {
 			for (let i = 0; i < storage.generate; i++) {
 				let key = storage.prefix;
-				console.log(key)
-				key += xxhashAsHex(storage.module + storage.storage + i).substr(2);
-				console.log(key)
-				content.genesis.raw.top[key] = key;
+				console.log("Prefix: ", key)
+				let addressBytes = deriveHard(i.toString())
+				console.log("Address Bytes: ", addressBytes)
+				let addressString = encodeAddress(addressBytes)
+				console.log("Address String: ", addressString)
+				let addressHex = u8aToHex(addressBytes);
+				console.log("Address Hex: ", addressHex)
+				let addressHash = blake2AsHex(addressBytes, 128);
+				console.log("Address Hash: ", addressHash)
+				key += addressHash.substr(2);
+				key += addressHex.substr(2);
+				console.log("Final Key: ", key)
+				content.genesis.raw.top[key] = "0x0000000000000064a7b3b6e00d0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 			}
 		}
 	}
