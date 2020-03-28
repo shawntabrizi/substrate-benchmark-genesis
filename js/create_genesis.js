1e829cda7fb3037bc762ea0e65265b6909d79fa7
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-28 23:20:43 +0100

    Mostly final stuff

--- js/create_genesis.js
+++ js/create_genesis.js
@@ -4,34 +4,53 @@ var { u8aToHex } = require('@polkadot/util');
 
 // Main function which needs to run at start
 function main() {
+	var args = process.argv;
+
 	const fs = require('fs');
-	const filename = 'polkadot-benchmark.json';
-	const content = require('../' + filename);
+	// This gets the filename from commandline
+	const filename = args[2];
+	const content = require('../input/' + filename + '.json');
 	const storageMetadata = require('../output/storage_metadata.json');
 
 	if (content.genesis.raw.top) {
 		for (storage of storageMetadata) {
 			for (let i = 0; i < storage.generate; i++) {
 				let key = storage.prefix;
-				console.log("Prefix: ", key)
-				let addressBytes = deriveHard(i.toString())
-				console.log("Address Bytes: ", addressBytes)
-				let addressString = encodeAddress(addressBytes)
-				console.log("Address String: ", addressString)
-				let addressHex = u8aToHex(addressBytes);
-				console.log("Address Hex: ", addressHex)
-				let addressHash = blake2AsHex(addressBytes, 128);
-				console.log("Address Hash: ", addressHash)
-				key += addressHash.substr(2);
-				key += addressHex.substr(2);
+				console.log("Prefix   : ", key)
+				let [suffix, value] = simple_fake_suffix_and_value(i);
+				key += suffix;
 				console.log("Final Key: ", key)
-				content.genesis.raw.top[key] = "0x0000000000000064a7b3b6e00d0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+				content.genesis.raw.top[key] = value;
 			}
 		}
 	}
 
 
-	fs.writeFileSync('./output/' + filename, JSON.stringify(content));
+	fs.writeFileSync('./output/' + filename + '-final.json', JSON.stringify(content));
+}
+
+function simple_fake_suffix_and_value(index) {
+	let suffix = xxhashAsHex(index.toString(), 128).substr(2);
+	let value = suffix;
+	return [suffix, value]
+}
+
+function real_account_suffix_and_value(index) {
+	let suffix = ""
+	let addressBytes = deriveHard(index.toString())
+	//console.log("Address Bytes: ", addressBytes)
+	let addressString = encodeAddress(addressBytes)
+	//console.log("Address String: ", addressString)
+	let addressHex = u8aToHex(addressBytes);
+	//console.log("Address Hex: ", addressHex)
+	let addressHash = blake2AsHex(addressBytes, 128);
+	//console.log("Address Hash: ", addressHash)
+	suffix += addressHash.substr(2);
+	suffix += addressHex.substr(2);
+
+	let value = "0x0000000000000064a7b3b6e00d0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
+	return [suffix, value]
 }
 
 main();
