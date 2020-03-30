c8b07ab72f0081e73d4521e3dd27ad03c7073bb3
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-30 15:08:38 +0200

    Use same keys as substrate

--- js/create_genesis.js
+++ js/create_genesis.js
@@ -1,23 +1,33 @@
-var { xxhashAsHex, deriveHard, blake2AsHex, encodeAddress } = require('@polkadot/util-crypto');
+var { xxhashAsHex, deriveHard, blake2AsHex, encodeAddress, mnemonicGenerate } = require('@polkadot/util-crypto');
 var { u8aToHex } = require('@polkadot/util');
+var { Keyring } = require('@polkadot/keyring');
+var { cryptoWaitReady } = require('@polkadot/util-crypto');
 
+const keyring = new Keyring({ type: 'sr25519' });
 
 // Main function which needs to run at start
-function main() {
+async function main() {
 	var args = process.argv;
+
+	await cryptoWaitReady();
+
 
 	const fs = require('fs');
 	// This gets the filename from commandline
 	const filename = args[2];
 	const content = require('../input/' + filename + '.json');
-	const storageMetadata = require('../output/storage_metadata.json');
+	const storageMetadataFilename = args[3];
+	if (!storageMetadataFilename) {
+		storageMetadataFilename = "storage_metadata"
+	}
+	const storageMetadata = require('../output/' + storageMetadataFilename + '.json');
 
 	if (content.genesis.raw.top) {
 		for (storage of storageMetadata) {
 			for (let i = 0; i < storage.generate; i++) {
 				let key = storage.prefix;
 				console.log("Prefix   : ", key)
-				let [suffix, value] = simple_fake_suffix_and_value(i);
+				let [suffix, value] = real_account_suffix_and_value(i);
 				key += suffix.substr(2);
 				console.log("Final Key: ", key)
 				content.genesis.raw.top[key] = value;
@@ -37,14 +47,18 @@ function simple_fake_suffix_and_value(index) {
 
 function real_account_suffix_and_value(index) {
 	let suffix = "0x"
-	let addressBytes = deriveHard(index.toString())
-	//console.log("Address Bytes: ", addressBytes)
-	let addressString = encodeAddress(addressBytes)
-	//console.log("Address String: ", addressString)
+	let dev_phrase = "bottom drive obey lake curtain smoke basket hold race lonely fit walk"
+
+	let user = keyring.addFromUri(dev_phrase + "//" + index.toString());
+
+	let addressBytes = user.publicKey;
+	console.log("Address Bytes: ", addressBytes)
+	let addressString = user.address;
+	console.log("Address String: ", addressString)
 	let addressHex = u8aToHex(addressBytes);
-	//console.log("Address Hex: ", addressHex)
+	console.log("Address Hex: ", addressHex)
 	let addressHash = blake2AsHex(addressBytes, 128);
-	//console.log("Address Hash: ", addressHash)
+	console.log("Address Hash: ", addressHash)
 	suffix += addressHash.substr(2);
 	suffix += addressHex.substr(2);
 
